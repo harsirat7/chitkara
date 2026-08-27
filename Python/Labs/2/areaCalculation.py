792eec1e2b9e7ab40931cf96a3acde95307ef28c
harsirat757 <harsiratsingh920@gmail.com>
2026-08-27 16:03:56 +0530

    CST LAb 4

---
+++ Python/Labs/2/areaCalculation.py
@@ -0,0 +1,13 @@
+print("Calculating Area Of Circle");
+
+radius = float(input("Enter Radius Of Circle In CM:"));
+pieValue=3.14;
+
+areaOfCircle = pieValue*(radius**2);
+print("Area of Cicle:",areaOfCircle,"CM");
+
+
+print("Area of Rectangle");
+length = float(input("Enter Length of Rectangle:"));
+breath = float(input("Enter Breath of Rectangle:"));
+print("Area of Rectangle:",length*breath);
---
+++ Python/Labs/2/calculations.py
@@ -0,0 +1,8 @@
+number1=float(input("Enter Your 1st Number:"));
+number2= float(input("Enter Your 2nd Number:"));
+
+print("Sum of Number:",number1+number2);
+print("Subtraction:",number1-number2);
+print("Multiplying:",number1*number2);
+print("Divide:",number1/number2);
+print("Remainder:",number1%number2);
